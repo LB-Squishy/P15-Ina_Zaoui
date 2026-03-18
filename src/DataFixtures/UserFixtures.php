e69037024790e53e5d84d255dad53e41f4e8642c
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2026-03-18 19:38:34 +0100

    Config/tests (#7)
    => Cafouillage dans les pull request oublie de merge feat(admin-invite) avant de créer la branche config(tests) : test de remise à propre
    
    - annulation pull request config(tests)
    - emission pull request feat(admin-invite)
    - re-emission pull request config(tests)
    
    * config(tests): Mise en place des script de gestion des DB + MAJ du ReadMe
    
    * config(tests): Mise en place des fixtures + DAMADoctrineTestBundle
    
    * config(tests): XDebug sous Docker + test-coverage
    
    * config(tests): upgrade PHPUnit: v9.5 => v11
    
    * config(tests): upgrade PHP 8.2 => php 8.3 // upgrade PHPUnit 11 => PHPUnit 12.5

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,11 +5,20 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $userPasswordHasher;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $description = "Le maître de l'urbanité capturée, explore les méandres des cités avec un regard vif et impétueux, figeant l'énergie des rues dans des instants éblouissants. À travers une technique avant-gardiste, il métamorphose le béton et l'acier en toiles abstraites, révélant l'essence même de l'architecture moderne. Ses clichés transcendent les formes familières pour révéler des perspectives inattendues, offrant une vision nouvelle et captivante du monde urbain.";
+
         // Création du compte d'Ina Zaoui
         $ina = new User();
         $ina->setSuperAdmin(true);
@@ -17,17 +26,23 @@ class UserFixtures extends Fixture
         $ina->setName('Ina Zaoui');
         $ina->setEmail('ina@zaoui.com');
         $ina->setDescription(null);
+        $ina->setPassword($this->userPasswordHasher->hashPassword($ina, 'password'));
         $manager->persist($ina);
 
-        // Création de 10 autres utilisateurs
-        foreach (range(1, 10) as $i) {
+        $this->addReference('user_ina', $ina);
+
+        // Création de 100 autres utilisateurs
+        foreach (range(1, 100) as $i) {
             $user = new User();
             $user->setSuperAdmin(false);
             $user->setAdmin(true);
             $user->setName(sprintf('Invité %d', $i));
             $user->setEmail(sprintf('invite+%d@example.com', $i));
-            $user->setDescription("Le maître de l'urbanité capturée, explore les méandres des cités avec un regard vif et impétueux, figeant l'énergie des rues dans des instants éblouissants. À travers une technique avant-gardiste, il métamorphose le béton et l'acier en toiles abstraites, révélant l'essence même de l'architecture moderne. Ses clichés transcendent les formes familières pour révéler des perspectives inattendues, offrant une vision nouvelle et captivante du monde urbain.");
+            $user->setDescription($description);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
             $manager->persist($user);
+
+            $this->addReference(sprintf('user_guest%d', $i), $user);
         }
 
         $manager->flush();
