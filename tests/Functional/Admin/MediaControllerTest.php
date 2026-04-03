62edd99623e5f4e58468a830942e3d23ca62353e
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2026-04-03 20:16:02 +0200

    Preprod: perf/frontOpti (#29)
    * feat(images): Optimisation avec des img au format webp + ajout du favicon
    
    * feat(guests): Ajout de la méthode findAdminGuestsWithMediaCount afin d'améliorer la charge de la requête
    
    * feat(guests): Ajout de la pagination à l'affichage de la liste d'invités
    
    * fix(guests): modification du test pour prise en compte du systeme de pagination
    
    * feat(guest): ajout du systeme de pagination + ajustement du test de la page
    
    * feat(portfolio): ajout du systeme de pagination + ajustement des tests
    
    * feat(head): ajout de lang="fr"

--- tests/Functional/Admin/MediaControllerTest.php
+++ tests/Functional/Admin/MediaControllerTest.php
@@ -70,6 +70,39 @@ class MediaControllerTest extends FunctionalTestCase
         $this->assertSame($expectedMediaPageCount, $mediaPageCount, 'Le nombre de pages de médias affichées ne correspond pas au nombre attendu.');
     }
 
+    ////////////////////////////////////////////////////////////////////-----TEST D'AFFICHAGE DE LA PAGE /ADMIN/MEDIA/ADD-----////////////////////////////////////////////////////////////////////
+    /**
+     * Test de l'affichage de la page /admin/media/add pour un super admin
+     */
+    public function testShowGoodAdminMediaAddPageForSuperAdmin(): void
+    {
+        $this->loginAsSuperAdmin();
+        $this->get('/admin/media/add');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorExists('select[name="media[user]"]', 'Le champ "user" devrait être visible pour les super admin');
+        $this->assertSelectorExists('select[name="media[album]"]', 'Le champ "album" devrait être visible pour les super admin');
+        $this->assertSelectorExists('input[name="media[title]"]', 'Le champ "title" devrait être visible pour les super admin');
+        $this->assertSelectorExists('input[name="media[file]"]', 'Le champ "file" devrait être visible pour les super admin');
+    }
+
+    /** 
+     * Test de l'affichage de la page /admin/media/add pour un admin
+     */
+    public function testShowGoodAdminMediaAddPageForAdmin(): void
+    {
+        $this->loginAsAdmin();
+        $this->get('/admin/media/add');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorNotExists('select[name="media[user]"]', 'Le champ "user" ne devrait pas être visible pour les admin');
+        $this->assertSelectorNotExists('select[name="media[album]"]', 'Le champ "album" ne devrait pas être visible pour les admin');
+        $this->assertSelectorExists('input[name="media[title]"]', 'Le champ "title" devrait être visible pour les admin');
+        $this->assertSelectorExists('input[name="media[file]"]', 'Le champ "file" devrait être visible pour les admin');
+    }
+
     ////////////////////////////////////////////////////////////////////-----TEST D'AJOUT DE MEDIA-----////////////////////////////////////////////////////////////////////
 
     /**
@@ -118,6 +151,10 @@ class MediaControllerTest extends FunctionalTestCase
         $media = null;
         $imagePath = null;
 
+        // Founir le chemin absolu du dossier uploads pour pouvoir nettoyer les fichiers après le test
+        $projectDirectory = $this->client->getContainer()->getParameter('kernel.project_dir');
+        $uploadsDirectory = $projectDirectory . '/public/';
+
         try {
             // Fournir le chemin de l'image de test
             $imagePath = __DIR__ . '/../../Images/image-test-ok.webp';
@@ -147,13 +184,87 @@ class MediaControllerTest extends FunctionalTestCase
             if ($user->isSuperAdmin()) {
                 $this->assertSame(1, $media->getAlbum()->getId(), 'Le média ajouté n\'est pas associé au bon album.');
             }
-            $this->assertFileExists($media->getPath(), 'Le fichier du média ajouté n\'a pas été trouvé dans le dossier uploads.');
+            $this->assertFileExists($uploadsDirectory . $media->getPath(), 'Le fichier du média ajouté n\'a pas été trouvé dans le dossier uploads.');
         } finally {
             // Nettoyer le dossier uploads après le test
-            if ($media && file_exists($media->getPath())) {
-                unlink($media->getPath());
+            if ($media) {
+                if (file_exists($uploadsDirectory . $media->getPath())) {
+                    unlink($uploadsDirectory . $media->getPath());
+                }
             }
         }
+    }
+
+    ////////////////////////////////////////////////////////////////////-----TEST D'AJOUT DE MEDIA-----////////////////////////////////////////////////////////////////////
+
+    /**
+     * Test d'ajout de media dépassant les 2 Mo
+     */
+    public function testCantAddMediaMoreThan2Mo(): void
+    {
+        $this->loginAsSuperAdmin();
+        $this->get('/admin/media/add');
+
+        $this->assertResponseIsSuccessful();
+
+        $media = null;
+        $imagePath = null;
+
+        // Fournir le chemin de l'image de test
+        $imagePath = __DIR__ . '/../../Images/image-more-than-2mo.jpg';
+
+        // Soumettre le formulaire d'ajout de média
+        $this->submitForm('Ajouter', [
+            'media[user]' => 1,
+            'media[album]' => 1,
+            'media[title]' => 'Test image 2mo',
+            'media[file]' => $imagePath,
+        ]);
+        $this->assertResponseIsSuccessful();
+
+        // Vérifier que le bon message d'erreur est affiché
+        $this->assertSelectorTextContains('.invalid-feedback', 'Sa taille ne doit pas dépasser 2 MB.', 'Le message d\'erreur correspondant n\'est pas affiché.');
+
+        // Vérifier que le média n'a pas été ajouté en BDD
+        $media = $this
+            ->service(MediaRepository::class)
+            ->findOneBy(['title' => 'Test image 2mo']);
+        $this->assertNull($media, 'Le média a été trouvé en BDD alors qu\'il ne devrait pas l\'être.');
+    }
+
+    /**
+     * Test d'ajout de media qui n'est pas une image
+     */
+    public function testCantAddMediaThatIsNotAnImage(): void
+    {
+        $this->loginAsSuperAdmin();
+        $this->get('/admin/media/add');
+
+        $this->assertResponseIsSuccessful();
+
+        $media = null;
+        $imagePath = null;
+
+        // Fournir le chemin de l'image de test
+        $imagePath = __DIR__ . '/../../Images/file-is-not-an-img.txt';
+
+        // Soumettre le formulaire d'ajout de média
+        $this->submitForm('Ajouter', [
+            'media[user]' => 1,
+            'media[album]' => 1,
+            'media[title]' => 'Test fichier txt',
+            'media[file]' => $imagePath,
+        ]);
+        $this->assertResponseIsSuccessful();
+
+        // Vérifier que le bon message d'erreur est affiché
+        $this->assertSelectorTextContains('.invalid-feedback', 'Les types autorisés sont "image/jpeg", "image/png", "image/webp"', 'Le message d\'erreur correspondant n\'est pas affiché.');
+
+        // Vérifier que le média n'a pas été ajouté en BDD
+        $media = $this
+            ->service(MediaRepository::class)
+            ->findOneBy(['title' => 'Test fichier txt']);
+        $this->assertNull($media, 'Le média a été trouvé en BDD alors qu\'il ne devrait pas l\'être.');
     }
 
     ////////////////////////////////////////////////////////////////////-----TEST DE SUPPRESSION DE MEDIA-----////////////////////////////////////////////////////////////////////
